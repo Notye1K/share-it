68a36655b26bd443711dc0098db1c7cd42a5f825
Kleyton <kleyton_vccamargo@hotmail.com>
2022-05-17 20:17:45 -0300

    feat: login and register working

--- src/Pages/Register/index.js
+++ src/Pages/Register/index.js
@@ -1,10 +1,77 @@
-import { Button, Container, Link } from '@mui/material'
+import { Visibility, VisibilityOff } from '@mui/icons-material'
+import {
+    Button,
+    Container,
+    FormControl,
+    IconButton,
+    InputAdornment,
+    InputLabel,
+    Link,
+    OutlinedInput,
+} from '@mui/material'
 import Box from '@mui/material/Box'
 import TextField from '@mui/material/TextField'
-import { Link as RouterLink } from 'react-router-dom'
+import { useContext, useState } from 'react'
+import { Link as RouterLink, useNavigate } from 'react-router-dom'
+
 import ToggleTheme from '../../Components/ToggleTheme'
+import AlertContext from '../../Contexts/AlertContext'
+import LoadingContext from '../../Contexts/LoadingContext'
+import { register } from '../../services/userService'
 
 export default function Register() {
+    const [values, setValues] = useState({
+        email: '',
+        nickName: '',
+        confirmPassword: '',
+        password: '',
+        showPassword: false,
+    })
+
+    const { setOpen, setMessage } = useContext(AlertContext)
+    const { stopLoading, startLoading } = useContext(LoadingContext)
+
+    const navigate = useNavigate()
+
+    const handleClickShowPassword = () => {
+        setValues({
+            ...values,
+            showPassword: !values.showPassword,
+        })
+    }
+
+    const handleChange = (prop) => (event) => {
+        setValues({ ...values, [prop]: event.target.value })
+    }
+
+    const handleMouseDownPassword = (event) => {
+        event.preventDefault()
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault()
+        startLoading()
+        const body = {
+            email: values.email,
+            nick: values.nickName,
+            password: values.password,
+        }
+        register(body)
+            .then(() => {
+                navigate('/login')
+            })
+            .catch((err) => {
+                if (err.response.status === 500) {
+                    setMessage('Houve um problema tente novamente mais tarde')
+                    setOpen(true)
+                } else {
+                    setMessage(err.response.data)
+                    setOpen(true)
+                }
+            })
+            .finally(stopLoading)
+    }
+
     return (
         <Box>
             <ToggleTheme />
@@ -16,27 +83,83 @@ export default function Register() {
                     width: '50%',
                     gap: '0.5em',
                 }}
+                onSubmit={handleSubmit}
             >
                 <TextField
                     id="outlined-basic"
                     label="Email"
                     variant="outlined"
+                    required
+                    value={values.email}
+                    type="email"
+                    onChange={handleChange('email')}
                 />
                 <TextField
                     id="outlined-basic"
                     label="NickName"
                     variant="outlined"
+                    required
+                    value={values.nickName}
+                    onChange={handleChange('nickName')}
                 />
-                <TextField
-                    id="outlined-basic"
-                    label="Senha"
-                    variant="outlined"
-                />
-                <TextField
-                    id="outlined-basic"
-                    label="Confirmar Senha"
-                    variant="outlined"
-                />
+                <FormControl variant="outlined">
+                    <InputLabel htmlFor="outlined-adornment-password">
+                        Senha
+                    </InputLabel>
+                    <OutlinedInput
+                        id="outlined-adornment-password"
+                        type={values.showPassword ? 'text' : 'password'}
+                        value={values.password}
+                        required
+                        onChange={handleChange('password')}
+                        endAdornment={
+                            <InputAdornment position="end">
+                                <IconButton
+                                    aria-label="toggle password visibility"
+                                    onClick={handleClickShowPassword}
+                                    onMouseDown={handleMouseDownPassword}
+                                    edge="end"
+                                >
+                                    {values.showPassword ? (
+                                        <VisibilityOff />
+                                    ) : (
+                                        <Visibility />
+                                    )}
+                                </IconButton>
+                            </InputAdornment>
+                        }
+                        label="Senha"
+                    />
+                </FormControl>
+                <FormControl variant="outlined">
+                    <InputLabel htmlFor="outlined-adornment-password">
+                        Confirmar Senha
+                    </InputLabel>
+                    <OutlinedInput
+                        id="outlined-adornment-password"
+                        type={values.showPassword ? 'text' : 'password'}
+                        value={values.confirmPassword}
+                        required
+                        onChange={handleChange('confirmPassword')}
+                        endAdornment={
+                            <InputAdornment position="end">
+                                <IconButton
+                                    aria-label="toggle password visibility"
+                                    onClick={handleClickShowPassword}
+                                    onMouseDown={handleMouseDownPassword}
+                                    edge="end"
+                                >
+                                    {values.showPassword ? (
+                                        <VisibilityOff />
+                                    ) : (
+                                        <Visibility />
+                                    )}
+                                </IconButton>
+                            </InputAdornment>
+                        }
+                        label="Confirmar Senha"
+                    />
+                </FormControl>
 
                 <Box
                     sx={{
--- src/services/userService.js
+++ src/services/userService.js
@@ -5,3 +5,7 @@ import { apiUrl } from './api'
 export function login(body) {
     return axios.post(apiUrl + `/login`, body)
 }
+
+export function register(body) {
+    return axios.post(apiUrl + `/register`, body)
+}
